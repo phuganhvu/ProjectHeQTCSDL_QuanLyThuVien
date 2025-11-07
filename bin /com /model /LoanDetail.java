1a266e1e5c3d793820db39456e32f3a87fd5a127
phuganhvu <vuphuonganh749@gmail.com>
2025-11-07 22:57:59 +0700

    Update LoanDetail.java

--- bin /com /model /LoanDetail.java
+++ bin /com /model /LoanDetail.java
@@ -1,1 +1,35 @@
+package com.nhom9.libraryapp.model;
+import java.sql.Date;
 
+
+public class LoanDetail extends Loan { 
+
+    private String bookTitle; 
+
+    // Constructors
+    public LoanDetail() {
+        super();
+    }
+
+   
+    public LoanDetail(int id, int userId, int bookId, Date ngayMuon, Date ngayTraDuKien, Date ngayTraThucTe, String trangThai, String bookTitle) {
+        super(id, userId, bookId, ngayMuon, ngayTraDuKien, ngayTraThucTe, trangThai); 
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "LoanDetail{" +
+               "loanInfo=" + super.toString() + 
+               ", bookTitle='" + bookTitle + '\'' +
+               '}';
+    }
+}
